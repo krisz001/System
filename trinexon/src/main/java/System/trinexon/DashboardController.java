26c3c65732be323eb67613adb3533f06cb5ee71f
Krisztián Bogdán <krisz001q@gmail.com>
2025-05-31 13:43:37 +0200

    Munkavállalói nyilvántartás

--- trinexon/src/main/java/System/trinexon/DashboardController.java
+++ trinexon/src/main/java/System/trinexon/DashboardController.java
@@ -1,14 +1,52 @@
 package System.trinexon;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class DashboardController {
 
     @FXML
     private Text felLabel;
 
+    @FXML
+    private ImageView teamImage;
+
     public void setUsername(String username) {
-        felLabel.setText("Bejelentkezett: " + username);
+        if (felLabel != null) {
+            felLabel.setText("Bejelentkezett: " + username);
+        }
     }
+
+    @FXML
+    public void initialize() {
+        if (teamImage != null) {
+            teamImage.setOnMouseClicked(this::openWorkerWindow);
+        }
+    }
+
+    @FXML
+    public void openWorkerWindow(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/workerView.fxml"));
+            Parent root = loader.load(); // AnchorPane -> Parent
+
+            Stage stage = new Stage();
+            stage.setTitle("Munkavállalói nyilvántartás");
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.APPLICATION_MODAL); // modal ablak
+            stage.showAndWait(); // várja az ablak bezárását
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
